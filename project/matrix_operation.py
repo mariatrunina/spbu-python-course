490477c907d4e227db16e64784a4dd5313202339
mariatrunina <mashatrunina2508@gmail.com>
2024-10-01 13:47:19 +0000

    Добавила docstring в операции над матрицами

--- project/matrix_operation.py
+++ project/matrix_operation.py
@@ -1,4 +1,17 @@
 def add_matrices(matrix1, matrix2):
+    """
+    Складывает две матрицы одинаковых размеров.
+
+    Args:
+        matrix1 (List[List[float]]): Первая матрица.
+        matrix2 (List[List[float]]): Вторая матрица.
+
+    Raises:
+        ValueError: Если матрицы имеют разные размеры или пустые.
+
+    Returns:
+        List[List[float]]: Результирующая матрица, являющаяся суммой matrix1 и matrix2.
+    """
     if (
         not matrix1
         or not matrix2
@@ -17,11 +30,22 @@ def add_matrices(matrix1, matrix2):
 
 
 def multiply_matrices(matrix1, matrix2):
-    # Проверка на пустые матрицы
+    """
+    Умножает две матрицы.
+
+    Args:
+        matrix1 (List[List[float]]): Первая матрица.
+        matrix2 (List[List[float]]): Вторая матрица.
+
+    Raises:
+        ValueError: Если одна из матриц пустая или если они несовместимы для умножения.
+
+    Returns:
+        List[List[float]]: Результирующая матрица, являющаяся произведением matrix1 и matrix2.
+    """
     if not matrix1 or not matrix2:
         raise ValueError("Матрицы не могут быть пустыми.")
 
-    # Проверка совместимости
     if len(matrix1[0]) != len(matrix2):
         raise ValueError("Несовместимые матрицы для умножения.")
 
@@ -35,7 +59,18 @@ def multiply_matrices(matrix1, matrix2):
 
 
 def transpose(matrix):
-    if not matrix:  # Проверка на пустую матрицу
+    """
+    Транспонирует матрицу.
+
+    Args:
+        matrix (List[List[float]]): Исходная матрица.
+
+    Returns:
+        List[List[float]]: Транспонированная матрица.
+        
+    Если входная матрица пустая, возвращает пустую матрицу.
+    """
+    if not matrix:  
         return []
 
     return [[matrix[j][i] for j in range(len(matrix))] for i in range(len(matrix[0]))]
