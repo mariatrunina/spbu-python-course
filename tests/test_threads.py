521bb1665aadfa8a813fdc1e2d2cb9d7d7712e8d
mariatrunina <mashatrunina2508@gmail.com>
2024-10-20 15:11:12 +0000

    подправила тесты

--- tests/test_threads.py
+++ tests/test_threads.py
@@ -21,11 +21,11 @@ def thread_pool():
 
 def test_enqueue_task(thread_pool):
     thread_pool.enqueue(sample_task)
-    assert thread_pool.tasks.qsize() == 1  # Проверка, что задача добавлена
+    assert thread_pool.tasks.qsize() == 1
 
 
 def test_thread_count(thread_pool):
-    assert len(thread_pool.threads) == thread_pool.pool_size  # Проверка размера пула
+    assert len(thread_pool.threads) == thread_pool.pool_size
 
 
 def test_task_completion(thread_pool):
@@ -34,27 +34,24 @@ def test_task_completion(thread_pool):
     def collect_result():
         result = sample_task()
         results.append(result)
-        return result
 
     for _ in range(10):
         thread_pool.enqueue(collect_result)
 
-    # Дождаться завершения всех задач перед вызовом dispose
-    thread_pool.tasks.join()  # Ожидание завершения всех задач
+    thread_pool.tasks.join()
 
-    # Теперь можно вызывать dispose, так как все результаты должны быть собраны
     thread_pool.dispose()
 
-    assert len(results) == 10  # Проверка, что все задачи выполнены
+    assert len(results) == 10
 
 
 def test_dispose(thread_pool):
     thread_pool.dispose()
-    assert thread_pool.is_disposed is True  # Проверка, что пул закрыт
+    assert thread_pool.is_disposed is True
 
 
 def test_cartesian_product_sum():
     list1 = [1, 2, 3]
     list2 = [4, 5, 6]
     result = compute_sum_with_futures(list1, list2)
-    assert result == 63  # Ожидаемое значение обновлено на 63
+    assert result == 63
